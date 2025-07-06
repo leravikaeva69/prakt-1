abec864587ba42ddd57bbe9918bebb4f3080c218
Наталия Иванова <leravikaeva69@gmail.com>
2025-07-06 17:06:27 +0700

    Add files via upload

---
+++ task5new.js
@@ -0,0 +1,12 @@
+const burgerMenuBtn=document.querySelector("#burger-menu-btn")
+const burgerMenu=document.querySelector("#burger-menu")
+const burgerMenuClose=document.querySelector("#burger-menu-close")
+
+
+burgerMenuBtn.addEventListener("click",() =>{
+    burgerMenu.style.left="0px"
+})
+burgerMenuBtn.addEventListener("click",() =>{
+ burgerMenu.style.left="-400px"
+
+})
