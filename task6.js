8d29ed6770e432975fae1a25dd90d017c0cb7460
Наталия Иванова <leravikaeva69@gmail.com>
2025-07-11 14:39:10 +0700

    Update task6.js

--- task6.js
+++ task6.js
@@ -1,24 +1,22 @@
-1 const bigImage=document querySelector("#big-image")
-2 const img1=document querySelector("#img-1")
-3 const img2=document querySelector("#img-2")
-4 const img3=document querySelector("#img-3")
-5 const img4=document querySelector("#img-4")
-6 const img5=document querySelector("#img-1")
+const bigImage = document.querySelector("#big-image");
+const img1 = document.querySelector("#img-1");
+const img2 = document.querySelector("#img-2");
+const img3 = document.querySelector("#img-3");
+const img4 = document.querySelector("#img-4");
+const img5 = document.querySelector("#img-5"); // Исправлено #img-1 на #img-5
 
-img1.addEventListener("click",()=>{
-bigImage.src=img1.src  
-})
-img2.addEventListener("click",()=>{
-bigImage.src=img2.src  
-})
-img3.addEventListener("clic"k,()=>{
-bigImage.src=img3.src 
-})
-img4.addEventListener("click",()=>{
-bigImage.src=img4.src  
-})
-img5.addEventListener("click",()=>{
-bigImage.src=img5.src  
- })
-
-
+img1.addEventListener("click", () => {
+    bigImage.src = img1.src;
+});
+img2.addEventListener("click", () => {
+    bigImage.src = img2.src;
+});
+img3.addEventListener("click", () => { // Исправлено "clic"k на "click"
+    bigImage.src = img3.src;
+});
+img4.addEventListener("click", () => {
+    bigImage.src = img4.src;
+});
+img5.addEventListener("click", () => {
+    bigImage.src = img5.src;
+});
