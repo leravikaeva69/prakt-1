0e4e7a0ac18c964d5119f8ad4962aa68031e7323
Наталия Иванова <leravikaeva69@gmail.com>
2025-07-11 13:45:04 +0700

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,24 @@
+1 const bigImage=document querySelector("#big-image")
+2 const img1=document querySelector("#img-1")
+3 const img2=document querySelector("#img-2")
+4 const img3=document querySelector("#img-3")
+5 const img4=document querySelector("#img-4")
+6 const img5=document querySelector("#img-1")
+
+img1.addEventListener("click",()=>{
+bigImage.src=img1.src  
+})
+img2.addEventListener("click",()=>{
+bigImage.src=img2.src  
+})
+img3.addEventListener("clic"k,()=>{
+bigImage.src=img3.src 
+})
+img4.addEventListener("click",()=>{
+bigImage.src=img4.src  
+})
+img5.addEventListener("click",()=>{
+bigImage.src=img5.src  
+ })
+
+
